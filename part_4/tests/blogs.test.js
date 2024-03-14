9ca5a53e039103f1baa73783f03cdaffa3f82f77
isakpulkki <isak@pulkki.me>
2024-03-14 16:12:12 +0200

    Exercises 4.6.-4.7.

--- part_4/tests/blogs.test.js
+++ part_4/tests/blogs.test.js
@@ -12,7 +12,7 @@ test('Dummy returns one.', () => {
 describe('Total likes...', () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
+      _id: '1',
       title: 'Kissakirja',
       author: 'Dani Huusanen',
       url: 'www.kissakirja.fi',
@@ -59,7 +59,7 @@ describe('Total likes...', () => {
 describe('Favorite blog...', () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
+      _id: '1',
       title: 'Kissakirja',
       author: 'Dani Huusanen',
       url: 'www.kissakirja.fi',
@@ -100,5 +100,117 @@ describe('Favorite blog...', () => {
   test('...When list has multiple blogs.', () => {
     const result = listHelper.favoriteBlog(listWithTwoBlogs)
     assert.strictEqual(result.likes, 8)
+  })
+})
+
+describe('Most blogs from author...', () => {
+
+  const listWithOneBlog = [
+    {
+      _id: '1',
+      title: 'Kissakirja',
+      author: 'Dani Huusanen',
+      url: 'www.kissakirja.fi',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '1',
+      title: 'Kissakirja',
+      author: 'Dani Huusanen',
+      url: 'www.kissakirja.fi',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Koirakirja',
+      author: 'Oskari Puusanen',
+      url: 'www.koirakirja.fi',
+      likes: 8,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Mursukirja',
+      author: 'Oskari Puusanen',
+      url: 'www.mursukirja.fi',
+      likes: 5,
+      __v: 0
+    },
+  ]
+
+  test('...When list is empty.', () => {
+    const result = listHelper.mostBlogs([])
+    assert.deepStrictEqual(result, {blogs: 0})
+  })
+
+  test('...When list has only one blog.', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, {author: 'Dani Huusanen', blogs: 1})
+  })
+
+  test('...When list has multiple blogs.', () => {
+    const result = listHelper.mostBlogs(listWithThreeBlogs)
+    assert.deepStrictEqual(result, {author: 'Oskari Puusanen', blogs: 2})
+  })
+})
+
+describe('Author with most likes...', () => {
+
+  const listWithOneBlog = [
+    {
+      _id: '1',
+      title: 'Kissakirja',
+      author: 'Dani Huusanen',
+      url: 'www.kissakirja.fi',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '1',
+      title: 'Kissakirja',
+      author: 'Dani Huusanen',
+      url: 'www.kissakirja.fi',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Koirakirja',
+      author: 'Oskari Puusanen',
+      url: 'www.koirakirja.fi',
+      likes: 8,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Mursukirja',
+      author: 'Oskari Puusanen',
+      url: 'www.mursukirja.fi',
+      likes: 5,
+      __v: 0
+    },
+  ]
+
+  test('...When list is empty.', () => {
+    const result = listHelper.mostLikes([])
+    assert.deepStrictEqual(result, {likes: 0})
+  })
+
+  test('...When list has only one blog.', () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    assert.deepStrictEqual(result, {author: 'Dani Huusanen', likes: 5})
+  })
+
+  test('...When list has multiple blogs.', () => {
+    const result = listHelper.mostLikes(listWithThreeBlogs)
+    assert.deepStrictEqual(result, {author: 'Oskari Puusanen', likes: 13})
   })
 })
--- part_4/utils/list_helper.js
+++ part_4/utils/list_helper.js
@@ -1,3 +1,6 @@
+const lodash = require('lodash');
+
+
 const dummy = (blogs) => {
     return 1
   }
@@ -17,9 +20,31 @@ const dummy = (blogs) => {
                 likes: blog.likes
             }
         }
-    }, {"likes": 0})
+    }, {likes: 0})
+  }
+
+  const mostBlogs = (blogs) => {
+    const grouped = lodash.groupBy(blogs, 'author')
+    const values = lodash.values(grouped)
+    const blogsByAuthor = values.map(groupedBlogs => ({ author: groupedBlogs[0].author, blogs: groupedBlogs.length}))
+    return blogsByAuthor.reduce((mostBlogs, blog) => {
+        if (blog.blogs > mostBlogs.blogs) {
+            return blog
+        }
+    }, {blogs: 0})
+  }
+
+  const mostLikes = (blogs) => {
+    const grouped = lodash.groupBy(blogs, 'author')
+    const values = lodash.values(grouped)
+    const likesByAuthor = values.map(groupedBlogs => ({ author: groupedBlogs[0].author, likes: lodash.sumBy(groupedBlogs, 'likes')}))
+    return likesByAuthor.reduce((mostLikes, blog) => {
+      if (blog.likes > mostLikes.likes) {
+          return blog
+      }
+  }, {likes: 0})
   }
 
   module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
   }
