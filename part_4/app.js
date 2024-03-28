df6bf0c1a425980fa1dec1e484cb8d8a0b0402f0
isakpulkki <isak@pulkki.me>
2024-03-28 17:48:55 +0200

    Reset path for part 5 Cypress -tests

--- part_4/app.js
+++ part_4/app.js
@@ -8,6 +8,7 @@ const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const resetRouter = require('./controllers/reset');
 
 mongoose.set('strictQuery', false);
 
@@ -22,9 +23,13 @@ mongoose
     logger.error('Error connecting to MongoDB.');
   });
 
+if (process.env.NODE_ENV === 'test') {
+    const resetRouter = require('./controllers/reset')
+    app.use('/api/reset', resetRouter)
+  } 
+
 app.use(cors());
 app.use(express.json());
-
 app.use(middleware.tokenExtractor);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
